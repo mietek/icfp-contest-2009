831190820f250093b5b01fec0fa2ebd5fa573d1e
Miëtek Bak <mietek@gmail.com>
2009-06-28 11:19:00 +0200

    Snapshot

--- Makefile
+++ Makefile
@@ -14,12 +14,18 @@ solve_all: vm solve
 	./solve 1002 > solved/1002.txt
 	./solve 1003 > solved/1003.txt
 	./solve 1004 > solved/1004.txt
+	./solve 2001 > solved/2001.txt
+	./solve 2003 > solved/2003.txt
+	./solve 2004 > solved/2004.txt
 
 trace_all: trace
 	./trace < solved/1001.txt > traced/1001.osf
 	./trace < solved/1002.txt > traced/1002.osf
 	./trace < solved/1003.txt > traced/1003.osf
 	./trace < solved/1004.txt > traced/1004.osf
+	./trace < solved/2001.txt > traced/2001.osf
+	./trace < solved/2003.txt > traced/2003.osf
+	./trace < solved/2004.txt > traced/2004.osf
 
 clean:
-	rm vm solve trace *\.hi *\.o solved/*\.txt traced/*\.osf
+	rm vm solve trace *\.hi *\.o
--- Task2.hs
+++ Task2.hs
@@ -1,5 +1,6 @@
 module Task2 (main) where
 
+import Debug.Trace (trace)
 import Tools
 import MoreTools
 import qualified VM as VM
@@ -11,34 +12,84 @@ runVM k i = (f, (x1, y1), (x1 - x2, y1 - y2), (x2, y2))
     [_, f, x1, y1, x2, y2] = VM.run 2 k i
 
 
-try :: Int -> Int -> (String, Double)
-try k t1 = (i3 ++ frame 1000 (0, 0), lenRV d3)
+test :: Int -> String -> (String, Double)
+test k i0 = (i2, lenRV d)
   where
     run = runVM k
-    i1 = frame t1 (0, 0)
-    (_, p1a, q1a, _) = run i1
-    (_, p1b, _, _) = run (i1 ++ frame 1 (0, 0))
-    r1 = lenRV p1a
-    r2 = lenRV q1a
-    v1 = satVelRV p1a p1b
-    vd1 = hohVDeltaRV v1 r1 r2
-    t2 = round (hohTime r1 r2)
-    i2 = i1 ++ frame 1 vd1 ++ frame t2 (0, 0)
-    (_, p2a, _, _) = run i2
-    (_, p2b, _, _) = run (i2 ++ frame 1 (0, 0))
-    v2 = satVelRV p2a p2b
-    vd2 = hohVDeltaRV' v2 r1 r2
-    i3 = i2 ++ frame 1 vd2
-    (_, _, _, d3) = run i3
+    runP = (\(_, p, _, _) -> p) . run
+    (_, p, q, _) = run i0
+    r1 = lenRV p
+    r2 = lenRV q
+    i1 = hohTrans runP r1 r2 i0
+    (_, _, _, d) = run i1
+    i2 = i1 ++ frame 1000 (0, 0)
+
+-- TODO: intelligent starting step
+
+-- largestPow10 :: Double -> Int
+-- largestPow10 x = 10^(fromIntegral (floor (logBase 10 x)))
+
+findBest :: Int -> String -> (Int, String, Double)
+findBest k i0 = (t5 + 1, i5, d5)
+  where
+    (_, d1) = test k i0
+    (_, d1') = test k (i0 ++ frame 1 (0, 0))
+    t2 = if d1 < d1'
+           then goAway k i0 1000 1 d1
+           else goToward k i0 1000 1 d1
+    (_, d2) = test k (i0 ++ frame t2 (0, 0))
+    t3 = goToward k i0 100 t2 d2
+    (_, d3) = test k (i0 ++ frame t3 (0, 0))
+    t4 = goToward k i0 10 t3 d3
+    (_, d4) = test k (i0 ++ frame t4 (0, 0))
+    t5 = goToward k i0 1 t4 d4
+    (i5, d5) = test k (i0 ++ frame (t5 + 1) (0, 0))
+
+goAway :: Int -> String -> Int -> Int -> Double -> Int
+goAway k i0 s t d =
+  trace ("a " ++ show t ++ " " ++ show d) $ if d < d'
+    then goAway k i0 s t' d'
+    else goToward k i0 s t' d'
+  where
+    t' = t + s
+    (_, d') = test k (i0 ++ frame t' (0, 0))
+
+goToward :: Int -> String -> Int -> Int -> Double -> Int
+goToward k i0 s t d =
+  trace ("t " ++ show t ++ " " ++ show d) $ if d < d'
+    then t - s
+    else goToward k i0 s t' d'
+  where
+    t' = t + s
+    (_, d') = test k (i0 ++ frame t' (0, 0))
+
+
+-- TODO: 2002 needs more precision
+-- we should check if we find a good enough solution (d < 1000)
+-- if not, maybe we should enter slower orbit (max r1 r2 * 1.25)?
+
+-- main :: Int -> String
+-- main k = show d
+--   where
+--     run = runVM k
+--     runP = (\(_, p, _, _) -> p) . run
+--     (_, p, q, _) = run (frame 1 (0, 0))
+--     r1 = lenRV p
+--     r2 = lenRV q
+--     i1 = if min r1 r2 / max r1 r2 < 0.5
+--       then hohTrans runP r1 (max r1 r2 * 0.7) (frame 1 (0, 0))
+--       else frame 1 (0, 0)
+--     (t, _, d) = findBest k i1
+
+
+main :: Int -> String
+main k = i
+  where
+    (_, i, _) = findBest k (frame 1 (0, 0))
+
 
 
 -- 2001: 15926
 -- 2002: 5416.5?
 -- 2003: 34140
 -- 2004: 14727
-
-
-main :: Int -> String
-main k = show d
-  where
-    (_, d) = try k 15926
